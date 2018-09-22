a39d7e3dcd90b663e27c5903f4eaa43e60e7a931
Tristan Brown <tgbrown@stanford.edu>
2018-09-22 05:18:26 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,8 @@
 class Author
   attr_accessor :name
 
+  post_count = 0
+
   def initialize (name)
     @name = name
     @posts = []
@@ -10,5 +12,11 @@ class Author
     @posts
   end
 
-  
+  def add_post(post)
+    @posts << post
+    post.authur = self
+    post_count += 1
+  end 
+
+
 end
