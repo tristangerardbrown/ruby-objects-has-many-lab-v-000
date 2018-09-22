e05f01d05f0ea55dbee09b6f860cde3fb7f0a182
Tristan Brown <tgbrown@stanford.edu>
2018-09-22 05:29:44 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,7 +1,30 @@
 class Post
   attr_accessor :author, :name
 
-  def initialize
+  @@all = []
+  
+  def initialize(title)
+    @title = title 
+    @@all << title 
   end
+
+  def all 
+    @@all
+  end 
+
+  def self.all 
+    @@all
+  end 
+
+  def title 
+    @title 
+  end 
+
+  def author 
+  end 
+
+  def author_name
+    self = author.name 
+  end 
   
 end
